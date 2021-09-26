c8837e5ecc4291f6c9e608e90a854499f6bee948
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-09-26 14:44:32 -0500

    MySQL database Setup
    MySQL database Setup

--- basic_poll_app/basic_poll_app/settings.py
+++ basic_poll_app/basic_poll_app/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'basic_poll_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'basic_poll_database',
+        'USER': 'root',
+        'PASSWORD': 'P)O(i8u7y6',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit' : True
+        }
     }
 }
 
