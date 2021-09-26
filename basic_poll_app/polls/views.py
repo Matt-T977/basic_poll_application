86d99f2c238b46cffa83aaf70007749ad51b05b8
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-09-26 16:58:26 -0500

    Beginning to create Views for each needed page for the application
    Beginning to create Views for each needed page for the application.

--- basic_poll_app/polls/views.py
+++ basic_poll_app/polls/views.py
@@ -1,3 +1,4 @@
+from django import http
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,3 +6,14 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, World. You're at the polls index!")
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+def result(request, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
+
